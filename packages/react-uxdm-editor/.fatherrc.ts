0efc362f3124725c31bb9ef1a21cd1a85d9eb503
arvinxx <arvinx@foxmail.com>
2020-11-23 16:12:54 +0800

    :wrench: chore: 调整发布配置

--- packages/react-uxdm-editor/.fatherrc.ts
+++ packages/react-uxdm-editor/.fatherrc.ts
@@ -1,5 +1,13 @@
 const base = require('../../.fatherrc');
 export default {
   ...base,
+  entry: 'src/index.tsx',
   pkgs: ['uxdm'],
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      { libraryName: 'antd', libraryDirectory: 'es', style: true },
+      'antd',
+    ],
+  ],
 };
--- packages/react-uxdm-editor/README.md
+++ packages/react-uxdm-editor/README.md
@@ -4,8 +4,8 @@
 
 ## Usage
 
-```
-const uxdmEditor = require('uxdm-editor');
+## TODO
 
-// TODO: DEMONSTRATE API
-```
+问题: 如何解决 stook-devtools?
+
+如何 解决 antd 的样式问题?
