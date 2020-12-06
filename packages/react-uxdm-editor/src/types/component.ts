4a52d78504709783c73cbbf6821099199a644541
arvinxx <arvinx@foxmail.com>
2020-12-06 15:52:43 +0800

    v0.0.1-beta.20

---
+++ packages/react-uxdm-editor/src/types/component.ts
@@ -0,0 +1,10 @@
+import { LayerNode } from 'uxdm';
+
+/**
+ *
+ */
+export type NodeTree = Record<string, LayerNode>;
+
+export type NodeList = Array<LayerNode>;
+
+export type LayerTree = string[];
